6d8ecf5a1f6620fd4874390c77d62411255a21f3
Mgdadali <109249013+Mgdadali@users.noreply.github.com>
2025-08-30 12:58:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def main():
     chrome_options.add_argument("--window-size=1920,1080")
     chrome_options.add_argument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114 Safari/537.36")
 
-    # تثبيت وتشغيل ChromeDriver 114
-    chromedriver_path = ChromeDriverManager(version="114.0.5735.90").install()
+    # تثبيت وتشغيل ChromeDriver تلقائيًا
+    chromedriver_path = ChromeDriverManager().install()
 
     driver = webdriver.Chrome(service=Service(chromedriver_path), options=chrome_options)
 
